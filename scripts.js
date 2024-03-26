d95eab4bec4668d87f28e164a9bd6630e5b2878e
juan salas <juanjo.salas.dev@gmail.com>
2024-03-26 19:48:55 +0100

    dom

---
+++ scripts.js
@@ -0,0 +1,30 @@
+console.log(document.title);
+
+let titulo = document.getElementById("gen-1");
+titulo.innerText = 'Generasión 1 Pokimon';
+
+let gen = document.querySelector(".infocard-list");
+gen.style.background = 'yellow';
+
+let url = document.URL;
+console.log(url);
+
+let domain = document.domain;
+console.log(domain);
+
+
+/*
+function cambiarImagenJS() {
+    document.querySelectorAll('img').src="https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+};
+
+function cambiarImagenJS() {
+    document.getElementsByTagName('img').src="https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+};
+
+let imagen = document.querySelectorAll('img');
+imagen.setAttribute('src','https://media.giphy.com/media/2v170e71aanfi/giphy.gif');
+
+tras investigar en google y probar todas las opciones que he visto y que se me ocurren sigo sin ver el problema
+*/
+
